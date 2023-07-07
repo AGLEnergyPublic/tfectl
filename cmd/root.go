47b1f931534fb5cd55f61c8040e042cdac6e2fe8
Sharath <sharathrnair87@gmail.com>
2023-07-07 13:09:23 +1000

    Fix/version (#35)
    * Bump version
    
    * 1.2.1

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ var rootCmd = &cobra.Command{
 	Use:               "tfectl",
 	Short:             "Query TFE from the command line.",
 	Long:              `Query TFE from the command line.`,
-	Version:           "v1.1.0",
+	Version:           "v1.2.1",
 	PersistentPreRunE: RunRootCmd,
 }
 
